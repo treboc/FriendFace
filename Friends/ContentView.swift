89ee4f0e0960c967886fc7530e413d774e9282cb
Marvin Lee <48476808+vogelfrey@users.noreply.github.com>
2022-04-04 19:20:58 +0200

    Initial Commit

---
+++ Friends/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Friends
+//
+//  Created by Marvin Lee Kobert on 04.04.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Friends/FriendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsApp.swift
+//  Friends
+//
+//  Created by Marvin Lee Kobert on 04.04.22.
+//
+
+import SwiftUI
+
+@main
+struct FriendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
